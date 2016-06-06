d424b3228c79f22e76bcee1bc09d7c52992b4e49
hhhhojeihsu.Mac <hhhhojeihsu@gmail.com>
2016-06-07 01:25:34 +0800

    see changes in description - fixed output format - add selected inside form

--- Back_End/server.js
+++ Back_End/server.js
@@ -62,12 +62,18 @@ function start() {
                         var html = "";
                         var header = '';
                         var body = '';
+                        var record = [];
+                        var ctr_selected = 0;
                         header += "<meta charset='UTF-8'><title>交通大學鋼琴社琴房預約系統</title><link rel='icon' href='Material/piano_icon.png'>";
 
                         /*  body    */
                         body += "您欲輸入的名字是"+ user_id;
                         /*  no record found  */
-                        if(rows.length == 0) console.log(user_id + " access system with no record found");
+                        if(rows.length == 0)
+                        {
+                            console.log(user_id + " access system with no record found");
+                            body += "您目前並沒有登記任何時段<br>";
+                        }
                         else    //show booked
                         {
                             console.log(user_id + " access system with " + rows.length + " record found");
@@ -75,18 +81,27 @@ function start() {
                             /*  booked  */
                             body += "<table>";
                             body += "<tr>" + "<td>" + "日期" + "</td>" + "<td>" + "時間" + "</td>" + "<td>" + "房號" + "</td>" + "</tr>";
-                            for (var ctr = 0; ctr < rows.length; ++ctr) {
+                            for (var ctr = 0; ctr < rows.length; ++ctr)
+                            {
+                                record[ctr] = ParseSqlDateCht(rows[ctr].date + "");
                                 body += "<tr>";
                                 /*  date    */
-                                body += "<td>" + rows[ctr].date + "</td>";
+                                body += "<td>";
+                                body += record[ctr].getFullYear() + "年 ";
+                                body += (record[ctr].getMonth() + 1).pad() + "月 ";
+                                body += record[ctr].getDate().pad() + "號 禮拜";
+                                body += day_cht.substring(record[ctr].getDay(), record[ctr].getDay() + 1);
+                                body += "</td>";
                                 /*  time    */
                                 body += "<td>";
-                                if (rows[ctr].time >= 12) {
-                                    if (rows[ctr].time != 12) body += (rows[ctr].time - 12) + ":00 PM ~ " + (rows[ctr].time - 11) + ":00 PM";
-                                    else body += rows[ctr].time() + ":00 PM ~ 1:00 PM";
+                                if (rows[ctr].time >= 12)
+                                {
+                                    if (rows[ctr].time != 12) body += (rows[ctr].time - 12).pad() + ":00 PM ~ " + (rows[ctr].time - 11).pad() + ":00 PM";
+                                    else body += rows[ctr].time.pad() + ":00 PM ~ 01:00 PM";
                                 }
-                                else {
-                                    body += rows[ctr].time + ":00 AM ~ " + (rows[ctr].time + 1) + ":00 AM";
+                                else
+                                {
+                                    body += rows[ctr].time.pad() + ":00 AM ~ " + (rows[ctr].time + 1).pad() + ":00 AM";
                                 }
                                 body += "</td>";
                                 /*  room    */
@@ -98,6 +113,7 @@ function start() {
                             }
                             body += "</table>";
                         }
+                        body += "<br>";
                         /*  point to the selectable form for user   */
                         body += "<table>";
                         body += "<tr>";
@@ -113,7 +129,7 @@ function start() {
                             }
                             else
                             {
-                                body += now.getMonth();
+                                body += now.getMonth() + 1;
                                 body += "/";
                                 body += FirstDayOfWeek.getDate() + ctr_day;
                             }
@@ -137,6 +153,7 @@ function start() {
                         }
                         body += "</tr>";
 
+                        body += "<form>";
                         for(var ctr_hr = 0; ctr_hr <= 23; ++ctr_hr)
                         {
                             body += "<tr>";
@@ -149,13 +166,34 @@ function start() {
                             else body += (ctr_hr + 1);
                             body += ":00";
                             body += "</td>";
-                            for(var ctr_day = 0; ctr_day < 7; ++ctr_day)
+                            for(var ctr_day = 0; ctr_day < 14; ++ctr_day)
                             {
                                 body += "<td>";
+                                body += "<input type='checkbox' name='";
+                                body += ctr_day;
+                                body += "_";
+                                body += ctr_hr;
+                                body += "' value='";
+                                body += ctr_day;
+                                body += "_";
+                                body += ctr_hr;
+                                body += "'";
+                                //TODO: IF SELECTED
+                                if(rows.length !== 0 && ctr_selected < rows.length)
+                                {
+                                    if(ctr_hr === rows[ctr_selected].time && ctr_day % 2 === rows[ctr_selected].room && FirstDayOfWeek.getDate() + Math.floor(ctr_day /2) === record[ctr_selected].getDate())
+                                    {
+                                        body += "checked";
+                                        ++ctr_selected;
+                                    }
+                                }
+                                body += ">";
                                 body += "</td>";
                             }
                             body += "</tr>";
                         }
+                        //TODO: INPUT BUTTON
+                        body += "</form>";
                         body += "</table>";
 
 
@@ -182,4 +220,48 @@ function start() {
         console.log('Server running at http://localhost:8888/');
     });
 }
+
+function ParseSqlDateCht(input)
+{
+    var string = "";
+    var date_obj = new Date();
+    var month = {
+        0: "Jan",
+        1: "Feb",
+        2: "May",
+        3: "Apr",
+        4: "Mar",
+        5: "Jun",
+        6: "Jul",
+        7: "Aug",
+        8: "Sep",
+        9: "Oct",
+        10: "Nov",
+        11: "Dec"};
+    /*  get Month */
+    string = input.substring(4, 7);
+    for(var ctr_mon = 0; ctr_mon < 12; ++ctr_mon)
+    {
+        if(input === month[ctr_mon])
+        {
+            date_obj.setMonth(ctr_mon);
+            break;
+        }
+    }
+    /*  get date    */
+    date_obj.setDate(Number(input.substring(8, 10)));
+    /*  get year    */
+    date_obj.setFullYear(Number(input.substring(11, 15)));
+    return date_obj;
+}
+
+/*	add leading zeroes	*/
+Number.prototype.pad = function(size)
+{
+    var s = String(this);
+    while (s.length < (size || 2)) s = "0" + s;
+    return s;
+}
+
+
 exports.start = start;
