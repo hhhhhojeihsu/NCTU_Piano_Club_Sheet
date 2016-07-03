023cbcf66ae15ec92b6948dbb34b6e5937e2f056
hhhhojeihsu.PC <hhhhojeihsu@gmail.com>
2016-07-04 00:03:45 +0800

    add ability to change the server to localhost or openshit's server, which changes the path including server.js, sql.js and html files.

--- Back_End/sql.js
+++ Back_End/sql.js
@@ -1,10 +1,22 @@
+/*  MODE SELECTION
+ 1 for openshift mode and 0 for localhost mode
+ //NOTE THAT THE MODE IN sql.js need to be changed as well
+ */
+
+var mode_selection = 1;
+var host_ = mode_selection ? process.env.OPENSHIFT_MYSQL_DB_HOST : 'localhost';
+var port_ = mode_selection ? process.env.OPENSHIFT_MYSQL_DB_PORT : '3306';
+var user_ = mode_selection ? process.env.OPENSHIFT_MYSQL_DB_USERNAME : 'root';
+var password_ = mode_selection ? process.env.OPENSHIFT_MYSQL_DB_PASSWORD : 'abc';
+var database_ = mode_selection ? 'noodejs' : 'info';
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host : process.env.OPENSHIFT_MYSQL_DB_HOST,
-    port : process.env.OPENSHIFT_MYSQL_DB_PORT,
-    user : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
-    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-    database : 'nodejs'
+    host : host_,
+    port : port_,
+    user : user_,
+    password : password_,
+    database : database_
 });
 
 
